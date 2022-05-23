ddf6e0957198c6df126e377eb916efd0153a8383
Paridhi Maheshwari <1997.paridhi@gmail.com>
2022-05-23 09:07:11 -0700

    distilbert for speeding up and similarity matrix

---
+++ create_similarity_matrix.py
@@ -0,0 +1,88 @@
+import torch
+import pickle
+import argparse
+import numpy as np
+from transformers import AutoTokenizer, AutoModel
+from sklearn.metrics.pairwise import cosine_similarity
+
+# load english dataset
+def load_data(path):
+	# return words list and labels
+	with open(path, 'r', encoding='utf-8') as f:
+		lines = f.readlines()
+		lines = [line.strip().lower().split('\t') for line in lines]
+		train_former = [line[0] for line in lines[:101171]]
+		train_quote = [line[1] for line in lines[:101171]]
+		train_latter = [line[2] for line in lines[:101171]]
+		valid_former = [line[0] for line in lines[101171:113942]]
+		valid_quote = [line[1] for line in lines[101171:113942]]
+		valid_latter = [line[2] for line in lines[101171:113942]]
+		test_former = [line[0] for line in lines[113942:]]
+		test_quote = [line[1] for line in lines[113942:]]
+		test_latter = [line[2] for line in lines[113942:]]
+		all_quotes = train_quote + valid_quote + test_quote
+	all_quotes = list(set(all_quotes))
+	all_quotes.sort()
+	y_train = [all_quotes.index(q) for q in train_quote]
+	y_valid = [all_quotes.index(q) for q in valid_quote]
+	y_test = [all_quotes.index(q) for q in test_quote]
+
+	return train_former, train_latter, train_quote, \
+			valid_former, valid_latter, valid_quote, \
+			test_former, test_latter, test_quote, \
+			torch.LongTensor(y_train), torch.LongTensor(y_valid), torch.LongTensor(y_test), all_quotes
+
+def mean_pooling(model_output, attention_mask):
+	# Take attention mask into account for correct averaging
+	token_embeddings = model_output[0] # First element of model_output contains all token embeddings
+	input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+	return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+
+def get_sentence_transformer_embeddings(sentences, pooling):
+	# Load model from HuggingFace Hub
+	tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+	model = AutoModel.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens')
+
+	# Tokenize sentences
+	encoded_input = tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+
+	# Compute token embeddings
+	with torch.no_grad():
+		model_output = model(**encoded_input)
+
+	if pooling == 'mean':
+		sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
+	elif pooling == 'cls':
+		sentence_embeddings = model_output[0][:, 0, :]
+
+	return sentence_embeddings
+
+def compute_similarity_matrix(sentence_embeddings):
+	similarities = cosine_similarity(sentence_embeddings)
+	return similarities
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--pooling", dest="pooling", action="store", type=str, choices=['cls', 'mean'])
+	args = parser.parse_args()
+
+	print('loading dataset......')
+	data_path = './data/english.txt'
+	train_former, train_latter, train_quote, valid_former, valid_latter, valid_quote, test_former, test_latter, test_quote, y_train, y_valid, y_test, all_quotes = load_data(data_path)
+
+	print('Getting quote embeddings')
+	all_quote_embeddings = get_sentence_transformer_embeddings(all_quotes, args.pooling)
+
+	print('Computing quote similarities')
+	all_quote_similarities = compute_similarity_matrix(all_quote_embeddings)
+	# Since cosine similarities varies from [-1, 1]
+	# Making it positive
+	all_quote_similarities += 1
+
+	final = {
+		'quotes' : all_quotes,
+		'similarities' : all_quote_similarities,
+	}
+	with open(f'data/similarities_sentence_bert_{args.pooling}.pkl', 'wb') as f:
+		pickle.dump(final, f)
---
+++ distilbert_english_sememe.py
@@ -0,0 +1,135 @@
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+from .modeling_bert import SememeEmbeddings
+
+class DistilBertSememeEmbeddings(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.word_embeddings = nn.Embedding(config.vocab_size, config.dim, padding_idx=config.pad_token_id)
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, config.dim)
+        if config.sinusoidal_pos_embds:
+            create_sinusoidal_embeddings(
+                n_pos=config.max_position_embeddings, dim=config.dim, out=self.position_embeddings.weight
+            )
+
+        self.LayerNorm = nn.LayerNorm(config.dim, eps=1e-12)
+        self.dropout = nn.Dropout(config.dropout)
+        self.sememe_embeddings = SememeEmbeddings(base='distilbert')
+
+    def forward(self, quote_ids, input_ids):
+        """
+        Parameters
+        ----------
+        input_ids: torch.tensor(bs, max_seq_length)
+            The token ids to embed.
+
+        Outputs
+        -------
+        embeddings: torch.tensor(bs, max_seq_length, dim)
+            The embedded tokens (plus position embeddings, no token_type embeddings)
+        """
+        seq_length = input_ids.size(1)
+        position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)  # (max_seq_length)
+        position_ids = position_ids.unsqueeze(0).expand_as(input_ids)  # (bs, max_seq_length)
+
+        word_embeddings = self.word_embeddings(input_ids)  # (bs, max_seq_length, dim)
+        position_embeddings = self.position_embeddings(position_ids)  # (bs, max_seq_length, dim)
+        sememe_embeds = self.sememe_embeddings(input_ids, quote_ids)
+        word_embeddings = torch.add(word_embeddings, sememe_embeds)
+
+        embeddings = word_embeddings + position_embeddings  # (bs, max_seq_length, dim)
+        embeddings = self.LayerNorm(embeddings)  # (bs, max_seq_length, dim)
+        embeddings = self.dropout(embeddings)  # (bs, max_seq_length, dim)
+        return embeddings
+
+class DistilBertSememeModel(DistilBertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.embeddings = DistilBertSememeEmbeddings(config)  # Embeddings
+        self.transformer = Transformer(config)  # Encoder
+
+        self.init_weights()
+
+    def get_input_embeddings(self):
+        return self.embeddings.word_embeddings
+
+    def set_input_embeddings(self, new_embeddings):
+        self.embeddings.word_embeddings = new_embeddings
+
+    def _prune_heads(self, heads_to_prune):
+        """ Prunes heads of the model.
+            heads_to_prune: dict of {layer_num: list of heads to prune in this layer}
+            See base class PreTrainedModel
+        """
+        for layer, heads in heads_to_prune.items():
+            self.transformer.layer[layer].attention.prune_heads(heads)
+
+    @add_start_docstrings_to_callable(DISTILBERT_INPUTS_DOCSTRING)
+    @add_code_sample_docstrings(tokenizer_class=_TOKENIZER_FOR_DOC, checkpoint="distilbert-base-uncased")
+    def forward(
+        self,
+        quote_ids,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        output_attentions=None,
+        output_hidden_states=None,
+    ):
+        r"""
+    Return:
+        :obj:`tuple(torch.FloatTensor)` comprising various elements depending on the configuration (:class:`~transformers.DistilBertConfig`) and inputs:
+        last_hidden_state (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the model.
+        hidden_states (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_hidden_states=True`` is passed or when ``config.output_hidden_states=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for the output of the embeddings + one for the output of each layer)
+            of shape :obj:`(batch_size, sequence_length, hidden_size)`.
+
+            Hidden-states of the model at the output of each layer plus the initial embedding outputs.
+        attentions (:obj:`tuple(torch.FloatTensor)`, `optional`, returned when ``output_attentions=True`` is passed or when ``config.output_attentions=True``):
+            Tuple of :obj:`torch.FloatTensor` (one for each layer) of shape
+            :obj:`(batch_size, num_heads, sequence_length, sequence_length)`.
+
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+        """
+        output_attentions = output_attentions if output_attentions is not None else self.config.output_attentions
+        output_hidden_states = (
+            output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
+        )
+
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        elif input_ids is not None:
+            input_shape = input_ids.size()
+        elif inputs_embeds is not None:
+            input_shape = inputs_embeds.size()[:-1]
+        else:
+            raise ValueError("You have to specify either input_ids or inputs_embeds")
+
+        device = input_ids.device if input_ids is not None else inputs_embeds.device
+
+        if attention_mask is None:
+            attention_mask = torch.ones(input_shape, device=device)  # (bs, seq_length)
+
+        # Prepare head mask if needed
+        head_mask = self.get_head_mask(head_mask, self.config.num_hidden_layers)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.embeddings(quote_ids, input_ids)  # (bs, seq_length, dim)
+        tfmr_output = self.transformer(
+            x=inputs_embeds,
+            attn_mask=attention_mask,
+            head_mask=head_mask,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+        )
+        hidden_state = tfmr_output[0]
+        output = (hidden_state,) + tfmr_output[1:]
+
+        return output  # last-layer hidden-state, (all hidden_states), (all attentions)
